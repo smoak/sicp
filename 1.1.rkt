d226de949937876ba588ba29552dadb347ed9d2a
Sasha Moak <sasha.moak@gmail.com>
2017-03-01 20:08:21 -0800

    Exercise 1.7...sort of

--- 1.1.rkt
+++ 1.1.rkt
@@ -82,3 +82,27 @@
 ;                     x)))
 
 ; This won't work because it has to evaluate both branches and so it will infinitely recurse
+
+; exercise 1.7
+(define (square x)
+  (* x x))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x)
+                 x)))
+
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
+
+; Not entirely sure about this exercise
